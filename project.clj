6c42901f5ec9283c8cf5d841550008d9fd31892d
Paul Williams <paul@paulswilliams.me.uk>
2016-11-20 18:29:44 +0000

    basic repo for playing with selenium in the clojure repl

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject selenium-google-search "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+[org.seleniumhq.selenium/selenium-java "2.53.1"]
+[org.seleniumhq.selenium/selenium-firefox-driver "2.53.1"]])
---
+++ src/selenium_google_search/core.clj
@@ -0,0 +1,6 @@
+(ns selenium-google-search.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
